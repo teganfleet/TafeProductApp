d7dbf95d20d7b2b137b10614dbbc9b638bed1b06
Tegan Fleet <tegan.fleet@student.tafesa.edu.au>
2023-12-06 11:54:11 +1030

    Commit the branch feat_GST1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalCharge.Text = Convert.ToString(cProduct.TotalPayment + 25);
                 TotalWrapCharge.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
+                TotalGST.Text = Convert.ToString((cProduct.TotalPayment + 25 + 5) * 1.1m);
             }
             catch (FormatException)
             {
